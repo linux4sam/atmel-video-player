be3557ff9e7b87bc01fe88eb8fce21a9693960fb
Joshua Henderson <joshua.henderson@microchip.com>
2017-11-09 15:56:18 -0700

    switch to using launch-fork.sh from application launcher
    Signed-off-by: Joshua Henderson <joshua.henderson@microchip.com>

--- Player.sh
+++ Player.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-/etc/init.d/S99demo stop
-/etc/init.d/S90weston stop
+. /opt/ApplicationLauncher/demo.config
+
+eval "$DEMO_LEAVE"
 
 QMLSCENE_DEVICE=softwarecontext \
 QT_LOGGING_RULES=qt.qpa.*=true \
@@ -12,5 +13,4 @@ QT_QPA_FB_DRM_FORMAT=18 \
 /opt/VideoPlayer/player -platform linuxfb -plugin tslib:/dev/input/event1 \
                -plugin evdevkeyboard:/dev/input/event0 $1
 
-/etc/init.d/S90weston start
-/etc/init.d/S99demo start
+eval "$DEMO_ENTER"
--- Video-Player.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# close stdout, stderr, stdin and double fork
-((/opt/VideoPlayer/Player.sh >&- 2>&- <&- &)&)
