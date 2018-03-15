55a956242e99c6fab0182ddd999fbd309de28b2f
Sandeep Sheriker Mallikarjun <sandeepsheriker.mallikarjun@microchip.com>
2018-03-15 15:47:47 -0700

    videoplayer: fix touch control's in video playback
    fixed the touch control's in video playback by removing
    tslib arguments point to event1 device.
    
    Signed-off-by: Sandeep Sheriker Mallikarjun <sandeepsheriker.mallikarjun@microchip.com>

--- Player.sh
+++ Player.sh
@@ -7,7 +7,7 @@ QT_QPA_EGLFS_KMS_CONNECTOR_INDEX=0 \
 QT_QPA_EGLFS_KMS_PLANE_INDEX=2 \
 QT_QPA_FB_DRM=1 \
 QT_QPA_FB_DRM_FORMAT=18 \
-/opt/VideoPlayer/player -platform linuxfb -plugin tslib:/dev/input/event1 \
-               -plugin evdevkeyboard:/dev/input/event0 $1
+/opt/VideoPlayer/player -platform linuxfb \
+        -plugin evdevkeyboard:/dev/input/event0 $1
 
 eval "$DEMO_ENTER"
